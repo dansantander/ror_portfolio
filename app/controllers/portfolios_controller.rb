9e1f6b16bc9f4301c0f55199660d30efc655d450
Daniel Santander <danyvo1d@gmail.com>
2020-05-16 22:12:31 -0500

    Integrate index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,17 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
 end
